07789447d1deaef653ea0b0667fb6995c2890cf1
Matthew Levandowski <levandowski.matthew@gmail.com>
2013-12-27 13:56:01 -0500

    updated readme again

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='pymassmailer',
       version=version,
       description="Mass Mailer using mallow mailer and jinja2 templates",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.md").read() + "\n" +
                        open(os.path.join("docs", "TODO.txt")).read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
